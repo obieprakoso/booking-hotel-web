3326c74dd10c055949f7889d99fb0727bf6c1f33
obie prakoso <obieprakoso@gmail.com>
2022-06-25 00:46:57 +0700

    add, header and navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="Logo">obie booking</span>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
